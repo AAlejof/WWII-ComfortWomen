1cae4cc3336dad863f6a7de1f1e08395b811d30b
Alejo Fariña <scaramouch11@gmail.com>
2023-08-17 17:19:24 -0300

    add basic structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,34 @@
-
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer';
+import Landing from './components/Landing/Landing';
+import Navbar from './components/Navbar/Navbar';
+import './App.css';
 
 function App() {
+  const location = useLocation();
+  const { pathname } = location;
+
+  const boolAddComponent =
+    pathname === '/' ||
+    pathname === '/home';
 
   return (
     <>
+      <div className="App">
+
+        {boolAddComponent && <Navbar />}
+
+        <Routes>
+          <Route path={'/'} element={<Landing />} />
+          <Route path={'/home'} element={<Home />} />
+        </Routes>
+
+        {boolAddComponent && <Footer />}
+      </div>
+
     </>
   )
 }
 
-export default App
+export default App;
