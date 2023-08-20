24a313b1135e365c7e8f9909bf85b8f1c2426524
Alejo Fariña <scaramouch11@gmail.com>
2023-08-20 17:01:33 -0300

    remove duplicated code

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,11 +4,6 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
