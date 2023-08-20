95f0fec66e1d912644b5cc0eff5204e73ef14da5
Alejo Fariña <scaramouch11@gmail.com>
2023-08-20 17:01:54 -0300

    add basic structure

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import style from './Footer.module.css';
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Footer;
