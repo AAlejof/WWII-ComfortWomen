9b43a91bf09391045227629e11eb0c5fd3d6a5d3
Alejo Fariña <scaramouch11@gmail.com>
2023-08-22 21:40:15 -0300

    change code to specific component

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,21 +1,13 @@
 import style from './Home.module.css';
 import { Link } from 'react-router-dom';
 import React from 'react';
-import image from '../../assets/img/homeImg.png'
+import ComfortWomen from '../ComfortWomen/ComfortWomen'
 
 const Home = () => {
     return (
         <>
             <div>
-                <div className={style.descImgDiv}>
-                    <div className={style.descDiv}>
-                        <h2 className={style.descH2}>Mujeres de <br />Confort</h2>
-                        <p className={style.descP}>Durante la Segunda Guerra Mundial, el término "Mujeres de Confort" se utilizó para encubrir a las mujeres esclavizadas sexualmente por el ejército japonés en Asia (1932-1945).</p>
-                    </div>
-                    <div>
-                        <img  className={style.descImg} src= {image} alt="Mujeres de confort" />
-                    </div>
-                </div>
+                <ComfortWomen/>
             </div>
         </>
     )
