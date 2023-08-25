4967e6f229ee3e8db284416f499f583b00e3c1bd
Alejo Fariña <scaramouch11@gmail.com>
2023-08-25 18:33:32 -0300

    add TruthJustice

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,12 +2,14 @@ import style from './Home.module.css';
 import { Link } from 'react-router-dom';
 import React from 'react';
 import ComfortWomen from '../ComfortWomen/ComfortWomen'
+import TruthJustice from '../Truth&Justice/Truth&Justice';
 
 const Home = () => {
     return (
         <>
             <div>
                 <ComfortWomen/>
+                <TruthJustice/>
             </div>
         </>
     )
