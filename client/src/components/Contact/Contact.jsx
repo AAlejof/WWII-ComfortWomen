e95614d8d25db7ce7eb2cdc9864eacca148c3d60
Alejo Fariña <scaramouch11@gmail.com>
2023-08-29 22:48:08 -0300

    add icons and form component

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,19 +1,39 @@
 import style from './Contact.module.css';
 import React, { useState } from 'react';
-import NewsCard from '../News/NewsCard/NewsCard';
+import ContactForm from './contactForm/contactForm';
+import instagram from '../../assets/img/icons/instagram.png';
+import facebook from '../../assets/img/icons/facebook.png';
+import linkedin from '../../assets/img/icons/linkedIn.png';
+import youtube from '../../assets/img/icons/youTube.png'
 
 const Contact = () => {
     return (
         <>
             <div>
-                <div className={style.descImgDiv}>
+                <div className={style.contactDiv}>
                     <div className={style.descDiv}>
                         <h2 className={style.descH2}>Contacto</h2>
                     </div>
                 </div>
                 <div className={style.infoDiv}>
                     <div className={style.contentDiv}>
-                        <NewsCard /><NewsCard /><NewsCard />
+                        <div className={style.formDiv}>
+                            <ContactForm/>
+                        </div>
+                        <div className={style.contactMeansDiv}>
+                            <p className={style.contactP}>
+                                Mail: <br /> 
+                                contacto@fundacionmanyoung.org <br /> 
+                                - <br /> 
+                                <a className={style.contactA} href="" >Fundación Manyoung</a>
+                            </p>
+                            <div className={style.contactIcons}>
+                                <img src={instagram} alt="instagram" />
+                                <img src={youtube} alt="youtube" />
+                                <img src={linkedin} alt="linkedin" />
+                                <img src={facebook} alt="facebook" />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
