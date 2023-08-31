661b00de7c04bf5b551463a1fe38884b97125ad4
Alejo Fariña <scaramouch11@gmail.com>
2023-08-31 20:19:00 -0300

    create db

---
+++ api/src/db.js
@@ -0,0 +1,39 @@
+const dotenv = require("dotenv");
+const { Sequelize } = require("sequelize");
+dotenv.config();
+
+const {
+	Admin,
+	Publication,
+} = require("./models/index");
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+
+// const { DB_DEPLOY } = process.env;
+
+const db = new Sequelize(
+	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+	{
+		logging: false, // set to console.log to see the raw SQL queries
+		native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+	}
+);
+
+// const db = new Sequelize(DB_DEPLOY, { logging: false, native: false });
+
+Admin(db);
+Publication(db);
+
+const {
+    admin,
+	publication
+} = db.models;
+
+
+admin.hasMany(publication);
+publication.belongsTo(admin);
+
+module.exports = {
+	...db.models,
+	conn: db,
+};
