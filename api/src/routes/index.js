be070ac4c9ef069f5e93d8d7781205b596a01b30
Alejo Fariña <scaramouch11@gmail.com>
2023-08-31 20:17:32 -0300

    add routes

---
+++ api/src/routes/index.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const router = Router();
+
+const publications = require('./publications.router');
+const admin = require('./admin.router');
+const mails = require('./mails.router');
+
+router.use('/publications', publications);
+router.use('/admin', admin);
+router.use('/mail', mails);
+
+
+module.exports = router;
