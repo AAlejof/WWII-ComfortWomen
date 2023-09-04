5ab8855801edfa811b1e36f839db63c97ba62420
Alejo Fariña <scaramouch11@gmail.com>
2023-09-04 16:28:17 -0300

    add controllers index

---
+++ api/src/controllers/index.js
@@ -0,0 +1,38 @@
+// Admin
+const deleteAdmin = require("./admin/deleteAdmin");
+const getAllAdmins = require("./admin/getAllAdmins");
+const postAdmin = require("./admin/postAdmin");
+const putAdmin = require("./admin/putAdmin");
+const restoreAdmin = require("./admin/restoreAdmin");
+const getSoftDeletedAdmin = require("./admin/getSoftDeletedAdmin");
+
+// Publications
+const createPublication = require("./publications/createPublication");
+const deletePublication = require("./publications/deletePublication");
+const editPublication = require("./publications/editPublication");
+const getAllPublications = require("./publications/getAllPublications");
+const getPublicationsByID = require("./publications/getPublicationsByID");
+const getPublicationsByName = require("./publications/getPublicationsByName");
+const restorePublication = require("./publications/restorePublication");
+const getSoftDeletedPublication = require("./publications/getSoftDeletedPublication");
+const filters = require("./publications/filters");
+
+module.exports = {
+	// Admin
+	deleteAdmin,
+	getAllAdmins,
+	postAdmin,
+	putAdmin,
+	restoreAdmin,
+	getSoftDeletedAdmin,
+    // Publications
+	createPublication,
+	deletePublication,
+	editPublication,
+	getAllPublications,
+	getPublicationsByID,
+	getPublicationsByName,
+	restorePublication,
+	getSoftDeletedPublication,
+	filters
+};
