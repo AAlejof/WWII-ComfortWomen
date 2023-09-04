e9cae6d7113e7d3c3cdc7984dfda8c70c5eb2316
Alejo Fariña <scaramouch11@gmail.com>
2023-09-04 16:30:07 -0300

    comment unused code

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,13 +1,13 @@
 const { Router } = require('express');
 const router = Router();
 
-const publications = require('./publications.router');
+//const publications = require('./publications.router');
 const admin = require('./admin.router');
-const mails = require('./mails.router');
+//const mails = require('./mails.router');
 
-router.use('/publications', publications);
+//router.use('/publications', publications);
 router.use('/admin', admin);
-router.use('/mail', mails);
+//router.use('/mail', mails);
 
 
 module.exports = router;
