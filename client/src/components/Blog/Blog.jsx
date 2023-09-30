cd61c6cc91582391a4d720a5cabe5289e2bd5ddb
Alejo Fariña <scaramouch11@gmail.com>
2023-09-29 23:34:51 -0300

    import components

--- client/src/components/Blog/Blog.jsx
+++ client/src/components/Blog/Blog.jsx
@@ -1,34 +1,30 @@
 import style from './Blog.module.css';
 import React, { useState } from 'react';
 import NewsCard from '../News/NewsCard/NewsCard';
-import image from '../../assets/img/news/newsImg1.png'
+import NewsDetail from '../News/NewsDetail/NewsDetail';
+import Contact from '../Contact/Contact';
 
 const Blog = () => {
     return (
         <>
-            <div className={style.descImgDiv}>
-                    <div className={style.descDiv}>
-                        <h2 className={style.descH2}>Mujeres de <br />Confort</h2>
-                        <p className={style.descP}>Durante la Segunda Guerra Mundial, el término "Mujeres de Confort" se utilizó para encubrir a las mujeres esclavizadas sexualmente por el ejército japonés en Asia (1932-1945).</p>
+            <div>
+                <NewsDetail />
+                <div className={style.otherNewsDiv}>
+                    <div className={style.descImgDiv}>
+                        <div className={style.descDiv}>
+                            <h2 className={style.descH2}>Otras noticias</h2>
+                        </div>
                     </div>
-                    <div className={style.carouselContainer}>
-                        <img className={style.descImg} src={image} alt="Mujeres de confort" />
+                    <div className={style.infoDiv}>
+                        <div className={style.contentDiv}>
+                            <NewsCard /><NewsCard /><NewsCard />
+                        </div>
+                        <button className={style.btnDiv}>
+                            VER MÁS
+                        </button>
                     </div>
                 </div>
-            <div className={style.otherNewsDiv}>
-                <div className={style.descImgDiv}>
-                    <div className={style.descDiv}>
-                        <h2 className={style.descH2}>Otras noticias</h2>
-                    </div>
-                </div>
-                <div className={style.infoDiv}>
-                    <div className={style.contentDiv}>
-                        <NewsCard /><NewsCard /><NewsCard />
-                    </div>
-                <button className={style.btnDiv}>
-                    VER MÁS
-                </button>
-                </div>
+                <Contact />
             </div>
         </>
 
