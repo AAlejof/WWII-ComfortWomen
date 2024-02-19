8ef529670ff19c5ea6c513879ca3a64044e2ef70
Alejo Fariña <scaramouch11@gmail.com>
2024-02-19 12:52:22 -0300

    add link to news card

--- client/src/components/News/NewsCard/NewsCard.jsx
+++ client/src/components/News/NewsCard/NewsCard.jsx
@@ -1,20 +1,23 @@
 import style from './NewsCard.module.css';
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import image from '../../../assets/img/news/newsImg1.png';
 
 const NewsCard = () => {
     return (
+        <Link to="/blog">
+            <div className={style.cardContainer}>
+                <div >
+                    <img className={style.cardImg} src={image} alt='News img' />
+                </div>
+                <h3 className={style.h3Title}>Muestra de fotografías “Para no volver” en la Legislatura de la Ciudad Autónoma de Buenos Aires.</h3>
+                <div className={style.cardDescription}>
+                    <p className={style.cardP}>Categoría</p>
+                    <p className={style.cardP}>12/01/23</p>
+                </div>
+            </div>
 
-        <div className={style.cardContainer}>
-            <div >
-                <img className={style.cardImg} src={image} alt='News img' />
-            </div>
-            <h3 className={style.h3Title}>Muestra de fotografías “Para no volver” en la Legislatura de la Ciudad Autónoma de Buenos Aires.</h3>
-            <div className={style.cardDescription}>
-                <p className={style.cardP}>Categoría</p>
-                <p className={style.cardP}>12/01/23</p>
-            </div>
-        </div>
+        </Link>
 
     )
 }
